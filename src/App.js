9eccbf9d417f5b95904eb3285ec70f82cff1d976
cp650497 <chinnikrishna.potnuru@broadcom.com>
2020-07-12 19:57:52 +0530

    Changed in feature branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Hi Chinni!!!
+          Hi Chinni!!! - Changed in Feature Branch
         </p>
         <a
           className="App-link"
